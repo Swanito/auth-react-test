6a40e900b8a97d99dd706da8539457245e0d79ed
Swanito <jon.gonzlez@gmail.com>
2023-06-13 15:47:18 +0200

    1 file modified, 1 added, 2 deleted

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-import AppRouter from './AppRouter';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import PasswordRecovery from './PasswordRecovery';
 
 ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <AppRouter />
-  </I18nextProvider>,
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={<LoginForm />} />
+          <Route path="/password-recovery" component={<PasswordRecovery />} />
+        </Switch>
+      </Router>
+    </I18nextProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
